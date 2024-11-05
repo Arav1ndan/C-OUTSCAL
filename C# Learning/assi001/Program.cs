e7d856ada464b9b2e07d1048beea863fbec0cd15
Aravindan <142475619+Arav1ndan@users.noreply.github.com>
2024-11-05 19:28:08 +0530

    Display the Story

---
+++ C# Learning/assi001/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("\n=================================================");
+Console.WriteLine("              MIDNIGHT PIZZA FIGHT              ");
+Console.WriteLine("=================================================");
+Console.WriteLine("In a bustling pizzeria on a busy Friday night...");
+Console.WriteLine("You, the Dought Master, created your magnum opus -\nthe perfect pizza suddenly, a sneaky Crust Bandit\nsnatches your masterpiece!");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine("\nFueled by passion for your craft, you give chase..");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine("Through winding alleys and crowded streets, you \npursue the pizza pilerer. Finally, the thief is \ncornered in a dead-end alley. It's time to recover \nyour stolen slice!");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine("                       FIGHT!                    ");
