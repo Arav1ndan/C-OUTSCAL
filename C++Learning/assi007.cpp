b3b72f317816e54dde3c3e09101dfecd981fe903
Aravindan <142475619+Arav1ndan@users.noreply.github.com>
2024-11-05 15:46:00 +0530

    Assignment - Developing health and damage system

--- C++Learning/assi007.cpp
+++ C++Learning/assi007.cpp
@@ -3,29 +3,55 @@ using namespace std;
 
 class Player
 {
-    private:
+private:
     int health = 100;
 
-    public:
-    void Move()
+    int minDamage = 15;
+    int maxDamge = 25;
+
+    int minHeal = 10;
+    int maxHeal = 15;
+
+public:
+    Player()
     {
-        cout << "The player can walk at 5km speed and run at 8km speed, he even can jump and sometimes he fly too.."<<endl;
+        cout << "The hero of this World..\n";
+        cout << "To save the world from evil..\n";
+        cout << "He is here...\n";
+    }
+
+    int GetHealth()
+    {
+        return health;
+    }
+    int GiveDamage(int damage)
+    {
+        if(health > 0)
+        {
+            cout <<"Player Health before getting damage: " << health <<endl;
+            health = health - damage;
+            cout <<" Player Health after getting damage: " << health << endl;
+        }else{
+            cout <<" ohh ohh Player died.. better luck next time.." <<endl;
+        }
+    }
+    int TakeDamage()
+    {
+
+    }
+    int Heal()
+    {
         
     }
+    void Move();
     void Fire();
-    Player();
-
+    
 };
-Player::Player()
-{
-    cout << "The hero of this World..\n";
-    cout<< "To save the world from evil..\n";
-    cout << "He is here...\n";
 
-}
+
 int main()
 {
     Player myPlayObj;
     myPlayObj.Move();
-    //myPlayObj.health;
+    // myPlayObj.health;
 }
